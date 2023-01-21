7b53567e2031de386285590c2950a99027092c6c
cFritchen <collin.fritchen@und.edu>
2023-01-21 15:20:06 -0600

    first commit

--- 1. JS Particles/index.js
+++ 1. JS Particles/index.js
@@ -3,57 +3,19 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// rectangles as a brush
-// canvas.addEventListener('mousemove', (e) => {
-//     ctx.beginPath();
-//     ctx.rect(e.x, e.y, 10, 10);
-//     ctx.fill();
-// })
-
-//creates circle
-// const degToRad = (deg) => {
-//     return (deg / 180) * Math.PI;
-// }; degrees to radians function
-
-// ctx.beginPath();
-// ctx.arc(200, 200, 100, degToRad(0), degToRad(360) );
-// ctx.fill();
-
-let atoms = [];
-
-canvas.addEventListener('click', function (e) {
-    for (let i = 0; i < 20; i++) {
-        atoms.push(new Atom(e.x, e.y));
-    }
+//rectangles as a brush
+canvas.addEventListener('mousemove', (e) => {
+    ctx.beginPath();
+    ctx.rect(e.x, e.y, 10, 10);
+    ctx.fill();
 })
 
-const animate = () => {
-    atoms.forEach(atom => {
-        atom.draw();
-        atom.update();
-    })
-    requestAnimationFrame(animate);
-}
+//creates circle
+const degToRad = (deg) => {
+    return (deg / 180) * Math.PI;
+}; //degrees to radians function
 
-animate();
-
-class Atom{
-    constructor(x,y){
-        this.x = x; 
-        this.y = y;
-        this.radius = Math.random() * 10 + 2;
-        this.speedX = Math.random() * 4 - 2;
-        this.speedY = Math.random() * 4 - 2;
-    }
-
-    update(){
-        this.x += this.speedX;
-        this.y += this.speedY;
-    }
-
-    draw(){
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
-        ctx.fill();
-    }
-}
+//creates circle
+ctx.beginPath();
+ctx.arc(200, 200, 100, degToRad(0), degToRad(360) );
+ctx.fill();
